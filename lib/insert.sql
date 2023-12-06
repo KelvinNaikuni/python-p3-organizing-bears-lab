c483d95c08c596f169a8ac6f11ec9e491a0a5b5b
Kelvin Naikuni <kelvin.naikuni@student.moringaschool.com>
2023-12-06 08:52:13 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+  ('Mr. Chocolate', 5, 'M', 'Brown', 'Friendly', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+  ('Tabitha', 4, 'F', 'White', 'Calm', 1),
+  ('Sergeant Brown', 6, 'M', 'Brown', 'Disciplined', 1),
+  ('Melissa', 2, 'F', 'Honey', 'Energetic', 1),
+  ('Grinch', 8, 'M', 'Green', 'Grumpy', 1),
+  ('Wendy', 7, 'F', 'Cinnamon', 'Sweet', 1),
+  (NULL, NULL, NULL, NULL, NULL, NULL);  -- Unnamed bear
